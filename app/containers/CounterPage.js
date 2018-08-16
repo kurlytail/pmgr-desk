becbc797b2b5e52600cb829241e31f61419bac32
ashwin <ashwinkn@hotmail.com>
2018-08-16 00:28:30 -0500

    Initial

---
+++ app/containers/CounterPage.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as CounterActions from '../actions/counter';
+import Counter from '../components/Counter';
+
+function mapStateToProps(state) {
+    return { counter: state.counter };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(CounterActions, dispatch);
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter);
